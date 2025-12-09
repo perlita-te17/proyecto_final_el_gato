d8d5bf58b654ce948ac75f27e085d8a7954cd451
perlita-te17 <perlisekivell@gmail.com>
2025-12-09 12:54:52 -0600

    tic tac toe mejorado

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -2,35 +2,37 @@ from tkinter import *
 import random 
 
 def next_turn(row, column):
+    
     global player
 
     if buttons[row][column]['text'] == '' and check_winner() is False:
+        
         if player == players[0]:
 
             buttons[row][column]['text'] = player
 
             if check_winner() is False:
                 player = players[1]
-                label.config(text=players[1]+"turn")
+                label.config(text=players[1]+" turn")
             elif check_winner() is True:
-                label.config(text=player[0]+"wins")
+                label.config(text=players[0]+" wins")
             elif check_winner() == 'Empate':
                 label.config(text="Empate!")
-    else: 
-        buttons[row][column]['text'] = player
+        else: 
+            buttons[row][column]['text'] = player
 
             if check_winner() is False:
                 player = players[0]
-                label.config(text=players[0]+"turn")
+                label.config(text=players[0]+" turn")
             elif check_winner() is True:
-                label.config(text=player[1]+"wins")
+                label.config(text=players[1]+" wins")
             elif check_winner() == 'Empate':
                 label.config(text="Empate!")                   
 
 
 def check_winner():
     for  row in range(3):
-        if buttons[row][1]['text'] == buttons[row][1]['text'] != '' :
+        if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != '' :
             buttons[row][0].config(bg='green')
             buttons[row][1].config(bg='green')
             buttons[row][2].config(bg='green')
@@ -38,30 +40,28 @@ def check_winner():
 
 
     for column in range(3):
-        if buttons[0][column]['text'] == buttons[1][column]['text'] != '':
+        if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != '':
             buttons[0][column].config(bg='green')
             buttons[1][column].config(bg='green')
             buttons[2][column].config(bg='green')
             return True
-
-
     if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != '':
-        buttons[0][0].config(bg='green')
-        buttons[1][1].config(bg='green')
-        buttons[2][2].config(bg='green')
-        return True 
-        elif  buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != '':
-           buttons[0][2].config(bg='green')
-           buttons[1][1].config(bg='green')
-           buttons[2][0].config(bg='green')
+            buttons[0][0].config(bg='green')
+            buttons[1][1].config(bg='green')
+            buttons[2][2].config(bg='green')
+            return True 
+    elif  buttons[0][2]['text'] == buttons[1][1]['text'] == buttons[2][0]['text'] != '':
+            buttons[0][2].config(bg='green')
+            buttons[1][1].config(bg='green')
+            buttons[2][0].config(bg='green')
             return True
-        elif empty_spaces() is False:
-            for row in range (3):
-                for column in range (3):
-                    buttons[row][column].config(bg='yellow')
-            return 'Empate' 
-        else:
-            return False        
+    elif empty_spaces() is False:
+        for row in range (3):
+            for column in range (3):
+                buttons[row][column].config(bg='yellow')
+        return 'Empate' 
+    else:
+        return False        
 
 def empty_spaces():
     
@@ -81,7 +81,7 @@ def new_game():
 
     for row in range(3):
         for column in range(3):
-            buttons[row][column].config(text='', bg='#F0F0F0')
+            buttons[row][column].config(text='', bg='#F5B427')
 
 window = Tk()
 window.title("Tic-Tac-Toe")
@@ -103,6 +103,6 @@ frame.pack()
 for row in range (3):
     for column in range (3):
         buttons[row][column] = Button(frame, text="",font=('consolas',40),width=5, height=2,
-                                        command=lambda row=row, column=column: next_turn(row,column))
+                                        command= lambda row=row, column=column: next_turn(row,column))
         buttons[row][column].grid(row=row, column=column)                                
 window.mainloop()
